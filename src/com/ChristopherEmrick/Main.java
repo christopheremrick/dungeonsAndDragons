fe384cd51e3bb1aa835ce6c63c624b0e020ecd45
Chris <christopher.emrick.2022@student.lhschools.org>
2019-03-21 20:30:20 -0400

    Created class selection

--- src/com/ChristopherEmrick/Main.java
+++ src/com/ChristopherEmrick/Main.java
@@ -1,8 +1,36 @@
 package com.ChristopherEmrick;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Classes
+
+        Scanner scan = new Scanner(System.in);
+
+        String userClass= "";
+
+        do {
+            System.out.println("What class do you want to be?");
+            System.out.println("Type Knight, Rouge, Archer, or Mage.");
+            userClass = scan.nextLine();
+
+            if (userClass.equals("Knight")) { // Knight class stats
+
+            } else if (userClass.equals("Rouge")) { // Rouge class stats
+                
+
+            }
+            else if (userClass.equals("Archer")) { // Archer class stats
+
+            }
+            else if (userClass.equals("Mage")) { // Mage class stats
+
+            }
+            else {
+                System.out.println("That isn't one of the classes!");
+            }
+        } while(!(userClass.equals("Rouge") || userClass.equals("Knight") || userClass.equals("Archer") || userClass.equals("Mage")));
     }
 }
